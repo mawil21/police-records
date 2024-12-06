fe671da2d95cdeaba9924c3938eebca72374c96d
Mawil Hasan <mawil0721@berkeley.edu>
2024-12-05 20:54:42 -0800

    updating gpt models to output json structure as a response

--- src/models/gpt_4_model.py
+++ src/models/gpt_4_model.py
@@ -2,15 +2,17 @@ from openai import OpenAI
 
 client = OpenAI()
 
-def chatGPT_api(message_content,temperature=0):
+def chatGPT_api(message_content):
     response = client.chat.completions.create(
         model = "gpt-4",
+        response_format={'type':'json_object'},
         messages = [
-            {"role": "user", "content": message_content}],
-        temperature = temperature
+            {"role" : "user", "content" : "Provide output in valid JSON."},
+            {"role": "user", "content": message_content}
+        ]
     )
-
     return response.choices[0].message.content
+
 
 def gpt_4(prompt):
     return chatGPT_api(prompt)
--- src/models/gpt_4o_model.py
+++ src/models/gpt_4o_model.py
@@ -2,16 +2,16 @@ from openai import OpenAI
 
 client = OpenAI()
 
-def chatGPT_api(message_content,temperature=0):
+def chatGPT_api(message_content):
     response = client.chat.completions.create(
         model = "gpt-4o",
+        response_format={'type':'json_object'},
         messages = [
-            {"role": "user", "content": message_content}],
-        temperature = temperature
+            {"role" : "user", "content" : "Provide output in valid JSON."},
+            {"role": "user", "content": message_content}
+        ]
     )
-
     return response.choices[0].message.content
-
 
 def gpt_4o(prompt):
     return chatGPT_api(prompt)
